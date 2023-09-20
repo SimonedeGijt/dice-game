34f8da7cb0e6559b657ed1e0559ba19af0d3d1bd
d-r-drost <dennis@kramphub.com>
2023-09-20 09:40:03 +0200

    colerfull logging

--- run.py
+++ run.py
@@ -1,7 +1,37 @@
 import logging
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+from colorlog import ColoredFormatter
+
+
+def setup_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    # Create console handler
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+
+    # Create formatter and add it to the handlers
+    formatter = ColoredFormatter(
+        "%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
+        datefmt=None,
+        reset=True,
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white',
+        },
+        secondary_log_colors={},
+        style='%'
+    )
+    ch.setFormatter(formatter)
+
+    # Add the handlers to the logger
+    logger.addHandler(ch)
+
 
 if __name__ == '__main__':
-    logger.info('hij werkt!')
+    setup_logger()
+    logging.info('It works!')
